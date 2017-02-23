ac34c851ead2693cc84f3dbdcaa7b31c5fb38263
root <logicmoo@gmail.com>
2017-02-23 09:24:56 -0800

    v0.0.8

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(dictoo).
 title('Dict-like OO Syntax').
 
-version('0.0.7').
+version('0.0.8').
 download('https://github.com/TeamSPoon/dictoo/releases/*.zip').
 
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
